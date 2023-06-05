62a919dd12839a73b7b04f440ec6d833f6cd8d80
Cyrus Lacson <lacsoncyrus@gmail.com>
2023-06-05 23:31:38 +0800

    changed JUNIT testrunner to TESTNG

--- src/test/java/Tools/TestRunner.java
+++ src/test/java/Tools/TestRunner.java
@@ -1,17 +1,15 @@
 package Tools;
 
-import org.junit.runner.RunWith;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features",glue= {"StepDefinitions"}
 					, monochrome = false
 					, plugin = { "pretty", "html:target/code-test-report.html" }
                     , tags = "@PositiveTest or @NegativeTest"
 				)
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 
 }
